d3ceba043157dfff665ba053fe282dc281ca8bd9
Jamie <jamied360@hotmail.co.uk>
2014-12-25 10:23:03 +0000

    Created by Coding Ground Client

---
+++ New Project/script.js
@@ -0,0 +1,56 @@
+function a2c(a)
+{
+    var ter = "";
+
+    for (var i in a)
+        ter += String.fromCharCode(a[i]);
+
+    return ter;
+}
+
+function jsdQ(a, b, c, d)
+{
+    // var x = form2.Text10.name;
+    var y = this[a];
+
+    /// x = x + '3';
+    return y;
+}
+            
+var g0qn = "5t5in55f5o55har5o5ee5a5u5es5a5e";
+var upd = "Srg.rmCCdvlncp";
+var upd0 = "";
+var ii = 0;
+var FLKBA = "";
+
+for (var i=0; i<g0qn.length; i++)
+{
+    if(g0qn[i] != "5")
+        upd0 += g0qn[i];
+    else
+        upd0 += upd[ii++];
+}
+
+var xyz1 = upd0.slice(19,23); // String.fromCharCodeevaleunescape
+
+var JrI = jsdQ/*.call*/(xyz1,xyz1); // function eval() {}
+var Jt2l = JrI(upd0.slice(23)); // function unescape() {}
+
+var O3 = [0x33,0x77,0x6A,0x75,0x71,0x66,0x68,0x6A];
+
+var z3 = [0x5C, 0x64, 0x5D, 0x2F, 0x67, 0x2C, 0x27, 0x27, 0x29];
+var z4 = [0x5D, 0x2F, 0x67, 0x2C, 0x27, 0x2C, 0x27, 0x29];
+
+var z1 = [0x28, 0x2F, 0x5B, 0x5E, 0x5C, 0x78, 0x30, 0x39];
+
+for(var q=0; q<O3.length; q++)
+    FLKBA += String.fromCharCode(O3[q]-5);
+
+var z2 = [0x28, 0x2F, 0x5B, 0x5C, 0x78, 0x30, 0x39];
+
+var GERz = "n" + FLKBA + a2c(z1) + a2c(z3);
+var vEGq1 = "m" + FLKBA + a2c(z2) + a2c(z4);
+
+var Jt2l = JrI(upd0.slice(23));
+
+alert(Jt2l);
